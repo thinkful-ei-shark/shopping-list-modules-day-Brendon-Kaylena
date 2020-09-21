4856168be63e6aafacb7ee009848bce0dcc0475e
Kaylena H <kaylenahall@gmail.com>
2020-09-21 09:10:36 -0700

    my attempts at step 6

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -68,15 +68,15 @@ const handleNewItemSubmit = function () {
 /* const toggleCheckedForListItem = function (id) {
   const foundItem = store.items.find(item => item.id === id);
   foundItem.checked = !foundItem.checked;
-};
+}; */
 
 const handleItemCheckClicked = function () {
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
     const id = getItemIdFromElement(event.currentTarget);
-    toggleCheckedForListItem(id);
+    store.findAndToggleChecked(id);
     render();
   });a
-}; */
+}; 
 
 const getItemIdFromElement = function (item) {
   return $(item)
@@ -88,10 +88,10 @@ const getItemIdFromElement = function (item) {
  * Responsible for deleting a list item.
  * @param {string} id
  */
-const deleteListItem = function (id) {
+/*const deleteListItem = function (id) {
   const index = store.items.findIndex(item => item.id === id);
   store.items.splice(index, 1);
-};
+}; */
 
 const handleDeleteItemClicked = function () {
   // like in `handleItemCheckClicked`, we use event delegation
@@ -99,16 +99,16 @@ const handleDeleteItemClicked = function () {
     // get the index of the item in store.items
     const id = getItemIdFromElement(event.currentTarget);
     // delete the item
-    deleteListItem(id);
+    store.findAndDelete(id);
     // render the updated shopping list
     render();
   });
 };
 
-const editListItemName = function (id, itemName) {
+/*const editListItemName = function (id, itemName) {
   const item = store.items.find(item => item.id === id);
   item.name = itemName;
-};
+}; */
 
 /**
  * Toggles the store.hideCheckedItems property
@@ -132,8 +132,8 @@ const handleEditShoppingItemSubmit = function () {
   $('.js-shopping-list').on('submit', '.js-edit-item', event => {
     event.preventDefault();
     const id = getItemIdFromElement(event.currentTarget);
-    const itemName = $(event.currentTarget).find('.shopping-item').val();
-    editListItemName(id, itemName);
+    const newName = $(event.currentTarget).find('.shopping-item').val();
+    store.findAndUpdateName(id, newName);
     render();
   });
 };
